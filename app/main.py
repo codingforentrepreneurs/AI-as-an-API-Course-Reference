a5868008de378dde41689252c664e1ce5a73aa5f
CFE <hello@teamcfe.com>
2021-10-03 22:48:14 -0500

    14 - Load Keras Model into FastAPI

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import pathlib
+import json
 from typing import Optional
 from fastapi import FastAPI
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+
 
 app = FastAPI()
 
@@ -12,10 +16,29 @@ MODEL_PATH = SMS_SPAM_MODEL_DIR / "spam-model.h5"
 TOKENIZER_PATH = SMS_SPAM_MODEL_DIR / "spam-classifer-tokenizer.json"
 METADATA_PATH = SMS_SPAM_MODEL_DIR / "spam-classifer-metadata.json"
 
+AI_MODEL = None
+AI_TOKENIZER = None
+MODEL_METADATA = {}
+labels_legend_inverted = {}
 
+
+@app.on_event("startup")
+def on_startup():
+    global AI_MODEL, AI_TOKENIZER, MODEL_METADATA, labels_legend_inverted
+    # load my model
+    if MODEL_PATH.exists():
+        AI_MODEL = load_model(MODEL_PATH)
+    if TOKENIZER_PATH.exists():
+        t_json = TOKENIZER_PATH.read_text()
+        AI_TOKENIZER = tokenizer_from_json(t_json)
+    if METADATA_PATH.exists():
+        MODEL_METADATA = json.loads(METADATA_PATH.read_text())
+        labels_legend_inverted = MODEL_METADATA['labels_legend_inverted']
 
 @app.get("/") # /?q=this is awesome
 def read_index(q:Optional[str] = None):
+    global AI_MODEL, MODEL_METADATA, labels_legend_inverted
     query = q or "hello world"
     # predict(query)
-    return {"query": query}
+    print(AI_MODEL)
+    return {"query": query, **MODEL_METADATA, "legend": labels_legend_inverted}
